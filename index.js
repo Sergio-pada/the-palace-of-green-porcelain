de7702e1490cfe6dfb9b10d9e2e7c21584ef9dee
Sergio Pada <pada.sergio@gmail.com>
2023-11-08 07:33:10 -0800

    Added EJS for exhibit pages

--- index.js
+++ index.js
@@ -1,42 +1,48 @@
 import pg from "pg";
 import express from "express";
-import path from "path"; // Import the 'path' module
-const app = express();
-
-import { dirname } from "path"; // No need to install path or url as they are native to node
+import path from "path";
+import { dirname } from "path";
 import { fileURLToPath } from "url";
+
+const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
+app.set('view engine', 'ejs'); // Set your view engine (EJS)
+app.set('views', path.join(__dirname, 'views')); // Set the directory for your view templates
+
+// Initialize the PostgreSQL client
+const client = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "archive",
+  password: "Guitarra90s!",
+  port: 5433,
 });
+app.use(express.static("public"));
+client.connect();
+// Serving static files from the "public" directory
+
+// Define the route for the exhibit pages
+app.get('/exhibit/:id', (req, res) => {
+  const exhibitId = req.params.id;
+
+  // Query the database to retrieve exhibit data based on exhibitId
+  client.query('SELECT * FROM exhibits WHERE exhibit_id = $1', [exhibitId], (err, dbRes) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error retrieving exhibit data');
+    } else {
+      // Render the exhibit page with the retrieved data
+      const exhibitData = dbRes.rows[0]; // Assuming a single exhibit with the given ID
+      res.render('exhibit-page', { exhibit: exhibitData });
+    }
+    console.log(req.params.image)
+  });
+});
+
+
 
 const port = 3000;
-
-app.use(express.static("public")); // This allows us to access static files from the "public" directory
-
-
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+  console.log(`Server started on port ${port}`);
 });
-
-const client = new pg.Client({
-    user: "postgres",
-    host: "localhost",
-    database: "archive",
-    password: "Guitarra90s!",
-    port: 5433,
-});
-
-client.connect();
-
-client.query('select * from users', (err, res) => {
-    if (!err) {
-        console.log(res.rows);
-    } else {
-        console.log(err.message);
-    }
-    client.end();
-});
-
-// REMINDER: Start again with command: npm run devStart
