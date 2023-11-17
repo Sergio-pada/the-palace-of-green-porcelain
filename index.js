fb317eab14d0ec780d6d4ea6079a79e08ad5fd62
Sergio Pada <pada.sergio@gmail.com>
2023-11-16 19:30:28 -0800

    Implemented cookies for user creation within database.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from "express";
 import path from "path";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import { v4 as uuidv4 } from 'uuid'; // for generating unique IDs
 
 const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -23,23 +24,79 @@ app.use(express.static("public"));
 client.connect();
 
 
-// Define the route for the exhibit pages
-app.get('/exhibit/:id', (req, res) => {
+
+
+// // Define the route for the exhibit pages
+// app.get('/exhibit/:id', (req, res) => {
+//   const exhibitId = req.params.id;
+  
+//   // Generate a unique ID for the new user
+//   const userId = uuidv4();
+//   console.log(userId);
+//   // Insert the new user into the database
+//   client.query('INSERT INTO users (user_id) VALUES ($1)', [userId], (err, dbRes) => {
+//     if (err) {
+//       console.error(err);
+//     } else {
+//       console.log('New user created with ID: ' + userId);
+//     }
+//   });
+
+//   // Query the database to retrieve exhibit data based on exhibitId
+//   client.query('SELECT * FROM exhibits WHERE exhibit_id = $1', [exhibitId], (err, dbRes) => {
+//     if (err) {
+//       console.error(err);
+//       res.status(500).send('Error retrieving exhibit data');
+//     } else {
+//       // Render the exhibit page with the retrieved data
+//       const exhibitData = dbRes.rows[0]; // Assuming a single exhibit with the given ID
+//       res.render('exhibit-page', { exhibit: exhibitData });
+//     }
+//     console.log(req.params.image)
+//   });
+// });
+
+
+import cookieParser from 'cookie-parser';
+
+
+app.use(cookieParser());
+
+app.get('/exhibit/:id', async (req, res) => {
   const exhibitId = req.params.id;
 
-  // Query the database to retrieve exhibit data based on exhibitId
-  client.query('SELECT * FROM exhibits WHERE exhibit_id = $1', [exhibitId], (err, dbRes) => {
-    if (err) {
+  let userId = req.cookies.userId;
+
+  if (!userId) {
+    // Generate a unique ID for the new user
+    userId = uuidv4();
+    console.log(userId);
+
+    // Insert the new user into the database
+    try {
+      const dbRes = await client.query('INSERT INTO users (user_id) VALUES ($1)', [userId]);
+      console.log('New user created with ID: ' + userId);
+
+      // Set a cookie to remember the user
+      res.cookie('userId', userId, { maxAge: 900000, httpOnly: true });
+    } catch (err) {
       console.error(err);
-      res.status(500).send('Error retrieving exhibit data');
-    } else {
-      // Render the exhibit page with the retrieved data
-      const exhibitData = dbRes.rows[0]; // Assuming a single exhibit with the given ID
-      res.render('exhibit-page', { exhibit: exhibitData });
     }
-    console.log(req.params.image)
-  });
+  }
+
+  // Query the database to retrieve exhibit data based on exhibitId
+  try {
+    const dbRes = await client.query('SELECT * FROM exhibits WHERE exhibit_id = $1', [exhibitId]);
+
+    // Render the exhibit page with the retrieved data
+    const exhibitData = dbRes.rows[0]; // Assuming a single exhibit with the given ID
+    res.render('exhibit-page', { exhibit: exhibitData });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error retrieving exhibit data');
+  }
 });
+
 
 
 
